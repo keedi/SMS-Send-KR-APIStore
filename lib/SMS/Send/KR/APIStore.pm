a72199aefe7858babaae4e452d7d54159390e676
Keedi Kim <keedi.k@gmail.com>
2015-06-02 17:01:29 +0900

    Add skeleton code for main module

---
+++ lib/SMS/Send/KR/APIStore.pm
@@ -0,0 +1,18 @@
+package SMS::Send::KR::APIStore;
+# ABSTRACT: An SMS::Send driver for the apistore.co.kr SMS service
+
+use strict;
+use warnings;
+use parent qw( SMS::Send::Driver );
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    ...
+
+
+=head1 DESCRIPTION
+
+...
